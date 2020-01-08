74199381ddaafa96177035358fe9d8f3b6f14d04
Edward Jung <megamorph12@gmail.com>
2020-01-08 17:04:58 -0500

    Replaced speedcontrollergroup

--- 2020/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ 2020/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -6,23 +6,35 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
+// import edu.wpi.first.wpilibj.SpeedController;
+// import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import frc.robot.Constants.DriveConstants;
 
 public class DriveTrain extends SubsystemBase {
-  private final SpeedController m_leftMotor = new SpeedControllerGroup(new CANSparkMax(DriveConstants.kLeftDriveMotor1Port, MotorType.kBrushless),
-      new CANSparkMax(DriveConstants.kLeftDriveMotor2Port, MotorType.kBrushless));
-  private final SpeedController m_rightMotor = new SpeedControllerGroup(new CANSparkMax(DriveConstants.kRightDriveMotor1Port, MotorType.kBrushless),
-      new CANSparkMax(DriveConstants.kRightDriveMotor2Port, MotorType.kBrushless));
+
+
+  // private final SpeedController m_leftMotor = new SpeedControllerGroup(new CANSparkMax(DriveConstants.kLeftDriveMotor1Port, MotorType.kBrushless),
+  //     new CANSparkMax(DriveConstants.kLeftDriveMotor2Port, MotorType.kBrushless));
+  // private final SpeedController m_rightMotor = new SpeedControllerGroup(new CANSparkMax(DriveConstants.kRightDriveMotor1Port, MotorType.kBrushless),
+  //     new CANSparkMax(DriveConstants.kRightDriveMotor2Port, MotorType.kBrushless));
+
+  private CANSparkMax m_leftMotor1;
+  private CANSparkMax m_rightMotor1;
+  private CANSparkMax m_rightMotor2;
 
   public DriveTrain() {
-
+    m_leftMotor1 = new CANSparkMax(DriveConstants.kLeftDriveMotor1Port, MotorType.kBrushless);
+    m_leftMotor2 = new CANSparkMax(DriveConstants.kLeftDriveMotor2Port, MotorType.kBrushless);
+    m_rightMotor1 = new CANSparkMax(DriveConstants.kRightDriveMotor1Port, MotorType.kBrushless);
+    m_rightMotor2 = new CANSparkMax(DriveConstants.kRightDriveMotor2Port, MotorType.kBrushless);
+    
+    m_leftMotor2.follow(m_leftMotor1);
+    m_rightMotor2.follow(m_rightMotor1);
   }
 
   public void drive(double left, double right) {
-    m_leftMotor.set(left);
-    m_rightMotor.set(right);
+    m_leftMotor1.set(left);
+    m_rightMotor1.set(right);
   }
 
   @Override
