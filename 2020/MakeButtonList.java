c62a29d39d04a9dea015c83ff70d8bd5b23aee15
TravisMadill <travismadill@gmail.com>
2020-02-17 12:28:07 -0500

    Finish making button list

--- 2020/MakeButtonList.java
+++ 2020/MakeButtonList.java
@@ -1,9 +1,14 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.regex.Pattern;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.regex.Matcher;
+import java.util.Collections;
 
 public class MakeButtonList {
 
@@ -11,11 +16,12 @@ public class MakeButtonList {
 	static HashMap buttonNames = new HashMap<String, String>();
 
 	public static void main(String[] args){
-		setUpButtonActions();
+		setUpButtonNames();
 		try {
 			String fileContents = Files.readString(Paths.get("src/main/java/frc/robot/RobotContainer.java"));
 			Pattern classPattern = Pattern.compile("\\{(.*)\\}$", Pattern.DOTALL);
 			Matcher classMatcher = classPattern.matcher(fileContents);
+			ArrayList<String> controlList = new ArrayList<String>();
 
 			if(classMatcher.find()){
 				String classBody = classMatcher.group(0);
@@ -32,10 +38,60 @@ public class MakeButtonList {
 					String functionBody = functionMatcher.group(1);
 					String[] lines = functionBody.split(";");
 					for(String s : lines){
-						System.out.println(s);
-						
+						// System.out.println(s);
+						Matcher controllerMatcher = Pattern.compile("(m_operatorController|m_driverController)").matcher(s);
+						if(controllerMatcher.find()){
+							String controller = controllerMatcher.group(1);
+							Matcher triggerMatcher = Pattern.compile("(JoystickButton|POVButton)").matcher(s);
+							if(triggerMatcher.find()){
+								String trigger = triggerMatcher.group(1);
+								Matcher buttonMatcher = Pattern.compile(trigger + "\\(.*?, (\\d+|Button\\.k.*\\.value)\\)").matcher(s);
+								if(buttonMatcher.find()){
+									String button = buttonMatcher.group(1);
+									button = button.replace("Button.k", "");
+									button = button.replace(".value", "");
+
+									Matcher commandMatcher = Pattern.compile("new .*\\(.*\\).*\\.(.*)\\(new (.*)\\((.*)\\)\\)").matcher(s);
+									if(commandMatcher.find()){
+										String action = commandMatcher.group(1);
+										String command = commandMatcher.group(2);
+										String commandArgsS = commandMatcher.group(3);
+										// System.out.printf("%s %s %s %s %s %s\n", getFormattedButtonString(controller), getFormattedButtonString(trigger), getFormattedButtonString(button), getFormattedButtonString(action), command, commandArgsS);
+
+										String[] commandArgs = commandArgsS.replace(" ", "").split(",");
+										ArrayList<String> commandsss = new ArrayList<String>();
+										for(String arg : commandArgs){
+											if(!arg.startsWith("m_")){
+												commandsss.add(arg);
+											}
+										}
+										if(commandsss.size() > 0){
+											String finalArgs = String.join(", ", commandsss);
+											String finalFunction = String.format("%s, %s %s: %s (%s)", getFormattedButtonString(controller), getFormattedButtonString(trigger).replace("***", getFormattedButtonString(button)), getFormattedButtonString(action), command, finalArgs);
+											System.out.println(finalFunction);
+											controlList.add(finalFunction);
+										} else {
+											String finalFunction = String.format("%s, %s %s: %s", getFormattedButtonString(controller), getFormattedButtonString(trigger).replace("***", getFormattedButtonString(button)), getFormattedButtonString(action), command);
+											System.out.println(finalFunction);
+											controlList.add(finalFunction);
+										}
+										
+									}
+								}
+							}
+						}
 					}
 				}
+
+				// Print file
+				BufferedWriter output = new BufferedWriter(new FileWriter("buttons.txt"));
+				// Collections.sort(controlList);	
+				Iterator<String> i = controlList.iterator();
+				while(i.hasNext()){
+					output.write(i.next());
+					output.write("\r\n");
+				}
+				output.close();
 			}
 		} catch(Exception e){
 			e.printStackTrace();
@@ -44,19 +100,36 @@ public class MakeButtonList {
 
 	static String getFormattedButtonString(String name){
 		if(buttonNames.containsKey(name)){
-			return buttonNames.get(name);
+			return buttonNames.get(name).toString();
 		} else return name;
 	}
 
-	static void setUpButtonNames(){
+	public static void setUpButtonNames(){
+		// Controllers
 		buttonNames.put("m_driverController", "Driver Controller");
 		buttonNames.put("m_operatorController", "Operator Controller");
-		buttonNames.put("JoystickButton", "Press the *** Button ");
-		buttonNames.put("POVButton", "Press *** on the D-Pad ");
+
+		// Buttons/triggers
+		buttonNames.put("JoystickButton", "*** Button");
+		buttonNames.put("POVButton", "D-Pad ***");
 		buttonNames.put("StickLeft", "Left Stick");
 		buttonNames.put("StickRight", "Right Stick");
 		buttonNames.put("BumperLeft", "Left Bumper");
 		buttonNames.put("BumperRight", "Right Bumper");
+
+		// Trigger actions
+		buttonNames.put("whileHeld", "(Hold)");
+		buttonNames.put("whenHeld", "(Hold, interruptable)");
+		buttonNames.put("toggleWhenPressed", "(Toggle on/off)");
+		buttonNames.put("whileActiveOnce", "(Hold, interruptable)");
+		buttonNames.put("whileActiveContinuous", "(Hold)");
+		buttonNames.put("whenReleased", "(When button is released)");
+		buttonNames.put("whenPressed", "(Runs command once, only on initial press)");
+		buttonNames.put("whenInactive", "(Runs when not held)");
+		buttonNames.put("whenActive", "(Activates on press, no cancel)");
+		buttonNames.put("toggleWhenActive", "(Toggles on/off)");
+		buttonNames.put("cancelWhenPressed", "(Cancels command when pressed)");
+		buttonNames.put("cancelWhenActive", "(Cancels command when activated)");
 
 		// D-Pad POV angles
 		buttonNames.put("0", "Up");
