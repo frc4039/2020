5b5af70382e91afad787b5365a7c52d8f7416533
taniadasilva17 <46635552+taniadasilva17@users.noreply.github.com>
2020-02-17 13:15:55 -0500

    stops v index

--- 2020/src/main/java/frc/robot/commands/Stir.java
+++ 2020/src/main/java/frc/robot/commands/Stir.java
@@ -42,6 +42,6 @@ public class Stir extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return !Feeder.getBreakBeam();
   }
 }
